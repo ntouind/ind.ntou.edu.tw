f0e99f6eab4fae493dcabd0a529fe175badb59d1
林博仁 <Buo.Ren.Lin@gmail.com>
2016-03-17 06:48:18 +0800

    新增自動拉入新版本的 GitHub Webhook
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

---
+++ GitHub Webhooks/README.markdown
@@ -0,0 +1,2 @@
+# GitHub Webhooks/
+這個目錄存放當有新 push 時自動觸發服務端進行 git pull 的程式
---
+++ GitHub Webhooks/拉入最新版本.php
@@ -0,0 +1,7 @@
+<?php 
+	system("git pull");
+	system("git lfs pull");
+	/* TODO: 確定這列是不是需要的 */
+	system("git lfs checkout");
+	
+?>
